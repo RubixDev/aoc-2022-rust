9c4b99abcaebaa60f9447ae36128dba8d7470649
RubixDev <silas.groh@t-online.de>
2022-12-06 08:57:48 +0100

    Add day 6

---
+++ src/day6.rs
@@ -0,0 +1,17 @@
+use itertools::Itertools;
+
+pub fn main() {
+    let input = include_bytes!("../inputs/day6.txt");
+    println!("--- Day 6 ---");
+    println!("Part 1: {}", find_unique_window(input, 4));
+    println!("Part 2: {}", find_unique_window(input, 14));
+    println!();
+}
+
+fn find_unique_window(input: &[u8], size: usize) -> usize {
+    input
+        .windows(size)
+        .take_while(|window| !window.iter().all_unique())
+        .count()
+        + size
+}
